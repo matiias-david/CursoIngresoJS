7455e517c656c1ef34600960e50c17716f718577
matiias-david <maty_lasirenaa@hotmail.com>
2018-02-05 16:37:37 -0300

    tarea

--- 1-EntradaSalida/jsEntradaSalida-3.js
+++ 1-EntradaSalida/jsEntradaSalida-3.js
@@ -2,7 +2,9 @@
 y luego mostrarlo por 'Alert' al presionar el botón  'MOSTRAR'*/
 function Mostar()
 {
-	
+    document.getElementById ("elNombre").value;
+    dato = document.getElementById("elNombre").value;
+    alert (dato)
 }
 
 
