02a1f31ed659d190be43ad16e7051a1f9f13ed50
matiias-david <maty_lasirenaa@hotmail.com>
2018-02-19 12:22:39 -0300

    lluvia3

--- 5-InstruccionWhile/jsIteraciones (5).js
+++ 5-InstruccionWhile/jsIteraciones (5).js
@@ -3,7 +3,7 @@ function Mostrar()
 
     var sexo = prompt("ingrese f ó m .");
     document.getElementById('Sexo').value=sexo;
-    while (sexo !="f" && sexo != 'm')
+    while (sexo !="f" && sexo != 'm' sexo != null)
     {
         alert ("Ingrese f o m")
 
