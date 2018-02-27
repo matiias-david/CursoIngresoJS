e3015f0ac8c91b8b68421d8852c8bb0f7a1ed350
matiias-david <maty_lasirenaa@hotmail.com>
2018-02-27 12:10:27 -0300

    a medias

--- 9-Parciales/parcial2/6-iteraciones.js
+++ 9-Parciales/parcial2/6-iteraciones.js
@@ -1,9 +1,13 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
 function Mostrar()
 {
-	var num = prompt ("Ingrese el dia de la semana que desee. ")
+	var importe = prompt ("Ingrese el dia de la semana que desee. ")
 	var bandera
-	
+	while ( importe < 0 )
+	{
+		importe = prompt ("")
+		importe = parseInt (importe)
+	}
 	
 }
 
