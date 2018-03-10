5f822377f8a520893f2385d7b5bb5a63e7c739f5
matiias-david <maty_lsairenaa@hotmail.com>
2018-03-10 12:58:07 -0300

    casi

--- 9-Parciales/parcial/cuatro.js
+++ 9-Parciales/parcial/cuatro.js
@@ -1,4 +1,22 @@
 function Mostrar()
 {
+    var num1;
+    var num2;
+    var mensaje;
+    
 
+    num1 = prompt ("Ingrese un numero ");
+    num2 = prompt ("ingrese un numero");
+    num1 = parseInt (num1)
+    num2 = parseInt (num2)
+
+    if ( num1 == num2)
+    {
+        mensaje = num1 +""+ num2
+        
+    }
+    if (num1 > num2){
+        mensaje = num1 - num2
+    }
+    else 
 }
--- 9-Parciales/parcial/ocho.js
+++ 9-Parciales/parcial/ocho.js
@@ -1,4 +1,30 @@
 function Mostrar()
 {
-
+    var contador = 0;
+    var respuesta = "continuar";
+    var temperatura;
+    var nombre;
+        while(respuesta != "no")//este es el inicio de la llave while
+        {
+            temperatura = prompt ("Ingrese temperatura: ")
+            temperatura = parseInt(temperatura);
+            while(temperatura < -100 || temperatura > 100 || isNaN(temperatura) )
+            {
+                temperatura = prompt ("Ingrese temperatura: ");
+                temperatura = parseInt(temperatura);
+            } 
+                nombre = prompt ("Ingrese un nombre: ")            
+                while ( ! isNaN(nombre))
+                {
+                    nombre = prompt ("Ingrese un nombre: ");
+                }
+            
+            
+            
+            
+            
+            
+            
+            respuesta = prompt ("Ingrese no para salir ");
+        }//termina la llave while (respuesta != "no")
 }
